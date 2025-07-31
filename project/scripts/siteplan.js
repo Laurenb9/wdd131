5171c5dcb0126ec2267bebdd7ea70f5ac6f49f54
Laurenb9 <laurendon906@gmail.com>
2025-07-31 16:49:38 -0500

    project siteplan added

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,9 @@
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    toggleButton.addEventListener("click", () => {
+      navMenu.classList.toggle("show");
+    });
+  });
+
